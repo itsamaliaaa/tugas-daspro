095cc38bd037c9d47f63d86d76a207bdbe128b05
itsamaliaaa <sitia.p24@gmail.com>
2024-11-08 16:04:43 +0700

    Menambahkan file procedure.py

---
+++ procedure.py
@@ -0,0 +1,14 @@
+# perbedaan antara procedure dan function
+
+# contoh procedure
+def penjumlahan(a,b):
+    hasil= a + b
+    return hasil
+
+penjumlahan(2,3) 
+
+# contoh function
+def greeting(nama):
+    print(f"Halo, {nama}!")
+
+greeting("Amalia")  
